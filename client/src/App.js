917334f0024c9ccd9152e5c140963e5d12964a5d
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-03-13 20:23:49 +0530

    removed pb-40 in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import Navbar from './components/layout/Navbar';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Points from './components/layout/Points';
-import Landing from './components/layout/Landing';
-import { loadUser } from './actions/auth';
-import store from './store';
-import setAuthToken from './utils/setAuthToken';
-import PrivateRoute from './utils/PrivateRoute';
-import Dashboard from './components/dashboard/Dashboard';
-import { connect } from 'react-redux';
-import Leaderboard from './components/layout/Leaderboard';
-import Evaluate from './components/layout/Evaluate';
-import FormLink from './components/layout/FormLink';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Points from "./components/layout/Points";
+import Landing from "./components/layout/Landing";
+import { loadUser } from "./actions/auth";
+import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
+import PrivateRoute from "./utils/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+import { connect } from "react-redux";
+import Leaderboard from "./components/layout/Leaderboard";
+import Evaluate from "./components/layout/Evaluate";
+import FormLink from "./components/layout/FormLink";
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -28,30 +28,30 @@ const App = ({ isAuthenticated }) => {
     <>
       <Router>
         <Fragment>
-          <div className='relative flex'>
-            <div className='flex-1 h-fit pb-40'>
+          <div className="relative flex">
+            <div className="flex-1 h-fit">
               <Navbar />
 
               <section>
                 {/* <Alert /> */}
                 <Routes>
-                  <Route exact path='/' element={<Landing />} />
+                  <Route exact path="/" element={<Landing />} />
                   {/* //modify this */}
-                  <Route exact path='/register' element={<Register />} />
-                  <Route exact path='/login' element={<Login />} />
+                  <Route exact path="/register" element={<Register />} />
+                  <Route exact path="/login" element={<Login />} />
 
                   {/*  after this*/}
                   <Route element={<PrivateRoute />}>
                     {/**private routes we can start from here what we wanto  */}
-                    <Route exact path='/dashboard' element={<Dashboard />} />
-                    <Route exact path='/points' element={<Points />} />
+                    <Route exact path="/dashboard" element={<Dashboard />} />
+                    <Route exact path="/points" element={<Points />} />
                     <Route
                       exact
-                      path='/leaderboard'
+                      path="/leaderboard"
                       element={<Leaderboard />}
                     />
-                    <Route exact path='/admin' element={<Evaluate />} />
-                    <Route exact path='/admin/form' element={<FormLink />} />
+                    <Route exact path="/admin" element={<Evaluate />} />
+                    <Route exact path="/admin/form" element={<FormLink />} />
                   </Route>
                 </Routes>
               </section>
