0fa28d699a8a40dd60ea796f04928539b01d3587
utkrisht5 <utkrishtsingh5@gmail.com>
2024-03-17 01:20:01 +0530

    Changing some files

--- backend/routes/api/points.js
+++ backend/routes/api/points.js
@@ -1,38 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs");
-const Team = require("../../models/Team");
-const Eval = require("../../models/Eval");
-const jwt = require("jsonwebtoken");
-const auth = require("../../middleware/auth");
-const config = require("config");
-const TotalPoints = require("../../models/TotalPoints");
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const Team = require('../../models/Team');
+const Eval = require('../../models/Eval');
+const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth');
+const config = require('config');
+const TotalPoints = require('../../models/TotalPoints');
 
-router.get("/", auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     console.log(req.team.id);
     const name = await Team.findById(req.team.id);
     const eval = await Eval.find({ teamName: name.teamName });
     res.json(eval);
-    console.log("Result of eval");
+    console.log('Result of eval');
     console.log(eval);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-router.get("/leaderboard", auth, async (req, res) => {
+router.get('/leaderboard', auth, async (req, res) => {
   try {
-    console.log("sdfasd");
+    console.log('sdfasd');
     const result = await Eval.aggregate([
       { $group: { _id: '$teamName', total: { $sum: '$points' } } },
-    ]).sort({points: -1});
+    ]).sort({ total: -1 });
     res.json(result);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
